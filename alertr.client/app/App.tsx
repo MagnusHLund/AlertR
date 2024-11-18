db5b46e1a4b13e114f0e0052d0511367ffa0212d
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2024-11-18 19:17:06 +0100

    Made UI until i ran "npm audit fix" and broke my project.

--- alertr.client/app/App.tsx
+++ alertr.client/app/App.tsx
@@ -1,31 +1,13 @@
 import React from 'react'
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  useColorScheme,
-  View,
-} from 'react-native'
+import { SafeAreaView, ScrollView, StatusBar, View } from 'react-native'
 
 import Header from './components/layout/Header'
 
-import { Colors } from 'react-native/Libraries/NewAppScreen'
-
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark'
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  }
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <SafeAreaView>
+      <Header />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Header />
         <View></View>
       </ScrollView>
     </SafeAreaView>
--- alertr.client/app/components/layout/Header.tsx
+++ alertr.client/app/components/layout/Header.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   ImageBackground,
   StyleSheet,
   Text,
   Dimensions,
   Image,
+  View,
 } from 'react-native'
 import CustomButton from '../input/Buttons/CustomButton'
 import CustomImage from '../content/CustomImage'
@@ -12,44 +13,65 @@ import CustomImage from '../content/CustomImage'
 const { height, width } = Dimensions.get('window')
 
 const Header = () => {
+  useEffect(() => {}, [])
+
   return (
-    <ImageBackground
-      style={[style.background, { height: height * 0.1 }]}
-      source={require('./../../assets/icons/placeholder.png')}
-    >
-      <CustomButton style={style.button} transparent={true}>
-        <CustomImage
-          src={require('./../../assets/icons/add.png')}
-          alt="Add new alert"
-        />
-      </CustomButton>
-      <Text style={style.title}>Welcome to AlertR</Text>;
-      <CustomButton style={style.button} transparent={true}>
-        <CustomImage
-          src={require('./../../assets/icons/settings.png')}
-          alt="Go to settings"
-        />
-      </CustomButton>
-    </ImageBackground>
+    <View>
+      <ImageBackground
+        style={[style.headerTop, style.bottomBorder]}
+        source={require('./../../assets/icons/AlertR-transparent.png')}
+        resizeMode="center"
+      >
+        <CustomButton style={style.button} transparent={true}>
+          <CustomImage
+            src={require('./../../assets/icons/add.png')}
+            alt="Add new alert"
+          />
+        </CustomButton>
+        <CustomButton style={style.button} transparent={true}>
+          <CustomImage
+            src={require('./../../assets/icons/settings.png')}
+            alt="Go to settings"
+          />
+        </CustomButton>
+      </ImageBackground>
+      <View style={[style.headerBottom, style.bottomBorder]}>
+        <Text style={style.pageTitle}>Home</Text>
+      </View>
+    </View>
   )
 }
 
 const style = StyleSheet.create({
-  background: {
-    justifyContent: 'space-evenly',
-    backgroundColor: 'black',
+  headerTop: {
+    justifyContent: 'space-between',
+    backgroundColor: '#eb9834',
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
+    height: height * 0.1,
   },
-  title: {
-    color: 'red',
+  pageTitle: {
     alignSelf: 'center',
     fontSize: 25,
+    fontWeight: 'bold',
   },
   button: {
     maxWidth: width * 0.1,
     maxHeight: width * 0.1,
+    marginHorizontal: width * 0.1,
+  },
+  headerBottom: {
+    height: height * 0.05,
+    backgroundColor: '#0390fc',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bottomBorder: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
   },
 })
 
